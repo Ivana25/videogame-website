ba5768e1eb65efaaf19eb6f11572426f0b6e188c
COR Students <corstudents@Matts-MBP-6.localdomain>
2018-11-20 12:26:04 -0500

    videogame-website

--- scripts.js
+++ scripts.js
@@ -1,17 +1,49 @@
-$("#click-button").click(function(){
-    const insertGame = $("#games").val()
-$.ajax({
-    url: `https://api-endpoint.igdb.com/${insertGame}/`,
-    success: function(data){
-        console.log(response.data)
-        $("#").html(data.game)
-        const pic = data.-.-
-        $("#pic").attr("src", pic)
+$("#lookUp").click(function(){
+    const gameInput = $("#game").val()
+    $.ajax({
+        url: `https://api-endpoint.igdb.com/games/?search=${gameInput}&fields=name,release_dates,summary,rating,screenshots&limit=5`,
+        headers: {
+            "user-key": "d2c7cfa12d4f83559bf5685dcba68ba6",
+             Accept: "application/json",
+        }
+    })
+        .then(response => {
+
+        //  console.log(response);
+        response.forEach(element => {
+            //  console.log(response)
+                // console.log(response)
+
+   let title = $("<h3>" + element.name + "<h3>")
+   $(document.body).append(title)
+   title.addClass('gameTitle')
+
+   let summary = $("<p>"+ element.summary + "<p>")
+   $(document.body).append(summary)
+   title.addClass('gameTitle')
+
+   let rating = $("<p>" + element.rating + "<p>")
+   $(document.body).append(rating)
+   title.addClass('gameTitle')
+
+   let pics = $("<p>" + element.screenshots + "<p>")
+   $(document.body).append('screenshots')
+   title.addClass('gameTitle')
+
+  let release_dates = element.release_dates      
+  release_dates.forEach(element => {
+        console.log(element.human)
+
+   let release_dates = $("<p>" + element.human + "<p>")
+   $(document.body).append(release_dates)
+   console.log(element.human)
+   title.addClass('gameTitle')
+              })
+         })
        
-    },
-    error: function(request, error)
-    {
-       alert("please name a videogame")
-    }
+              .catch(e => {
+                alert("This is not a videogame. Please try again.", e);
+              })
+              
 });
 })
